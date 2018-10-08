4dd3f773d38f3271ce6b910838db923cd457e6c3
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-10-08 13:55:41 +0100

    Fix tests

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/FlywayConfig.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/FlywayConfig.java
@@ -1,28 +1,26 @@
 package uk.gov.justice.digital.hmpps.oauth2server.config;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 @Configuration
 public class FlywayConfig {
 
-    private final DataSource authDataSource;
-
-    @Autowired
-    public FlywayConfig(@Qualifier("authDataSource") DataSource authDataSource) {
-        this.authDataSource = authDataSource;
-    }
-
-    @PostConstruct
-    public void migrateFlyway() {
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(authDataSource);
-        flyway.setLocations("db/auth/migration");
+    @Bean(initMethod = "migrate")
+    @FlywayDataSource
+    @Primary
+    public Flyway primaryFlyway(@Qualifier("authDataSource") DataSource authDataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(authDataSource)
+                .load();
         flyway.migrate();
+        return flyway;
     }
+
 }
--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/OAuth2AuthorizationServerConfig.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/OAuth2AuthorizationServerConfig.java
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.oauth2server.config;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,16 +12,13 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
@@ -31,10 +27,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
-import javax.sql.DataSource;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Configuration
@@ -45,34 +38,28 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
     private static final int HOUR_IN_SECS = 60 * 60;
     private final Resource privateKeyPair;
-    private final List<OauthClientConfig> oauthClientConfig;
     private final String keystorePassword;
     private final String keystoreAlias;
-    private final DataSource authDataSource;
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
+    private final ClientDetailsService clientDetailsService;
 
 
     @Autowired
     public OAuth2AuthorizationServerConfig(@Lazy AuthenticationManager authenticationManager,
-                                           @Lazy @Qualifier("authDataSource") DataSource authDataSource,
                                            @Lazy UserDetailsService userDetailsService,
-                                           PasswordEncoder passwordEncoder,
+                                           ClientDetailsService clientDetailsService,
                                            @Value("${jwt.signing.key.pair}") String privateKeyPair,
                                            @Value("${jwt.keystore.password}") String keystorePassword,
-                                           @Value("${jwt.keystore.alias:elite2api}") String keystoreAlias,
-                                           @Value("${oauth.client.data}") String clientData,
-                                           ClientConfigExtractor clientConfigExtractor) {
+                                           @Value("${jwt.keystore.alias:elite2api}") String keystoreAlias
+                                           ) {
 
         this.privateKeyPair = new ByteArrayResource(Base64.decodeBase64(privateKeyPair));
         this.keystorePassword = keystorePassword;
         this.keystoreAlias = keystoreAlias;
-        this.oauthClientConfig = clientConfigExtractor.getClientConfigurations(clientData);
         this.userDetailsService = userDetailsService;
         this.authenticationManager = authenticationManager;
-        this.authDataSource = authDataSource;
-        this.passwordEncoder = passwordEncoder;
+        this.clientDetailsService = clientDetailsService;
     }
 
     @Bean
@@ -82,7 +69,7 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(clientDetailsService());
+        clients.withClientDetails(clientDetailsService);
     }
 
     @Bean
@@ -132,50 +119,8 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
         defaultTokenServices.setSupportRefreshToken(true);
         defaultTokenServices.setAccessTokenValiditySeconds(HOUR_IN_SECS); // default 1 hours
         defaultTokenServices.setRefreshTokenValiditySeconds(HOUR_IN_SECS * 12); // default 12 hours
-        defaultTokenServices.setClientDetailsService(clientDetailsService());
+        defaultTokenServices.setClientDetailsService(clientDetailsService);
         defaultTokenServices.setAuthenticationManager(authenticationManager);
         return defaultTokenServices;
-    }
-
-    @Bean
-    public JdbcClientDetailsService clientDetailsService() {
-        JdbcClientDetailsService jdbcClientDetailsService = new JdbcClientDetailsService(authDataSource);
-        jdbcClientDetailsService.setPasswordEncoder(passwordEncoder);
-
-        if (oauthClientConfig != null && jdbcClientDetailsService.listClientDetails().size() == 0) {
-            for (OauthClientConfig client : oauthClientConfig) {
-                log.info("Initialising OAUTH2 Client ID {}", client.getClientId());
-
-                BaseClientDetails cd = new BaseClientDetails();
-                cd.setClientId(client.getClientId());
-                cd.setClientSecret(client.getClientSecret());
-
-                if (client.getAccessTokenValidity() != null) {
-                    cd.setAccessTokenValiditySeconds(client.getAccessTokenValidity());
-                }
-                if (client.getRefreshTokenValidity() != null) {
-                    cd.setRefreshTokenValiditySeconds(client.getRefreshTokenValidity());
-                }
-                if (client.getScope() != null) {
-                    cd.setScope(client.getScope());
-                }
-                if (client.getAutoApprove() != null) {
-                    cd.setAutoApproveScopes(client.getAutoApprove());
-                }
-                if (client.getAuthorities() != null) {
-                    cd.setAuthorities(client.getAuthorities().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
-                }
-                if (client.getAuthorizedGrantTypes() != null) {
-                    cd.setAuthorizedGrantTypes(client.getAuthorizedGrantTypes());
-                }
-                if (client.getWebServerRedirectUri() != null) {
-                    cd.setRegisteredRedirectUri(client.getWebServerRedirectUri());
-                }
-
-                jdbcClientDetailsService.addClientDetails(cd);
-            }
-        }
-
-        return jdbcClientDetailsService;
     }
 }
