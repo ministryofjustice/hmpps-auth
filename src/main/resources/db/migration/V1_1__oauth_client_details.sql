9d79c14a7f1728577901160c07fb8ab86e5fe1ce
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-10-08 17:22:50 +0100

    Support SQL Server

--- src/main/resources/db/migration/V1_1__oauth_client_details.sql
+++ src/main/resources/db/migration/V1_1__oauth_client_details.sql
@@ -1,16 +1,15 @@
 DROP TABLE IF EXISTS oauth_client_details;
 
 create table oauth_client_details (
-  client_id               varchar(64)  not null,
-  access_token_validity   bigint,
+  client_id               varchar(64)  not null primary key,
+  access_token_validity   INT,
   additional_information  varchar(255),
   authorities             varchar(255),
   authorized_grant_types  varchar(200) not null,
   autoapprove             varchar(200),
   client_secret           varchar(100) not null,
-  refresh_token_validity  bigint,
+  refresh_token_validity  INT,
   resource_ids            varchar(255),
   scope                   varchar(200),
-  web_server_redirect_uri varchar(255),
-  primary key (client_id)
+  web_server_redirect_uri varchar(255)
 );
