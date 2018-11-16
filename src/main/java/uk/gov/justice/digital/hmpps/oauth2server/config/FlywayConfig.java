02f886bb03db7f0738af9a03aec086189a8bb884
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-11-16 18:17:22 +0000

    Java 11 Spring Boot 2.1 Remove config

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/FlywayConfig.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/FlywayConfig.java
@@ -2,12 +2,14 @@ package uk.gov.justice.digital.hmpps.oauth2server.config;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class FlywayConfig {
@@ -15,9 +17,11 @@ public class FlywayConfig {
     @Bean(initMethod = "migrate")
     @FlywayDataSource
     @Primary
-    public Flyway primaryFlyway(@Qualifier("authDataSource") DataSource authDataSource) {
+    public Flyway primaryFlyway(@Qualifier("authDataSource") DataSource authDataSource,
+        @Value("${auth.flyway.locations:db/migration}") List<String> flywayLocations) {
         Flyway flyway = Flyway.configure()
                 .dataSource(authDataSource)
+                .locations(flywayLocations.toArray(new String[0]))
                 .load();
         flyway.migrate();
         return flyway;
