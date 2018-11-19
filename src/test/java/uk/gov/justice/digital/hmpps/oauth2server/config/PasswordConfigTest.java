210d18962cb01bb474c1e9ce8666bd4fe14ada93
steverendell <steve.rendell@syscon.net>
2018-11-19 15:56:57 +0000

    Add custodyapi to in-memory test data

---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/config/PasswordConfigTest.java
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.oauth2server.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("dev")
+public class PasswordConfigTest {
+    @Autowired
+    private PasswordConfig passwordConfig;
+
+    @Test
+    public void encodePassword() {
+        final String encoded = passwordConfig.passwordEncoder().encode("some_password_123456");
+        assertThat(encoded.length()).isEqualTo(60);
+    }
+}
