350bb89bec9e1fb96952655fc2aedccfd12b35f0
Peter Phillips <peter@greenthistle.co.uk>
2018-12-05 10:35:01 +0000

    NN-1467: Remove ClientDetail entity as not used

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/auth/repository/ClientDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.oauth2server.auth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.gov.justice.digital.hmpps.oauth2server.auth.model.ClientDetail;
-
-public interface ClientDetailRepository extends CrudRepository<ClientDetail, String> {
-
-}
