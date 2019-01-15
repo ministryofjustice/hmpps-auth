6859b2a5ebda5087f0dde9a76f0752486e1ca002
Peter Phillips <peter@greenthistle.co.uk>
2019-01-15 15:34:40 +0000

    NN-1488: Keep user on selection page if no selection is chosen

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/resource/VerifyEmailController.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/resource/VerifyEmailController.java
@@ -42,8 +42,13 @@ public class VerifyEmailController {
     }
 
     @GetMapping("/verify-email")
-    public ModelAndView verifyEmailRequest(final Principal principal, final HttpServletRequest request, final HttpServletResponse response) throws IOException, ServletException {
+    public ModelAndView verifyEmailRequest(final Principal principal, final HttpServletRequest request, final HttpServletResponse response,
+                                           @RequestParam(required = false) final String error)
+            throws IOException, ServletException {
         final var modelAndView = new ModelAndView("verifyEmail");
+        if (StringUtils.isNotBlank(error)) {
+            modelAndView.addObject("error", error);
+        }
 
         // Firstly check to see if they have an email address
         final var username = principal.getName();
@@ -75,8 +80,13 @@ public class VerifyEmailController {
 
     @PostMapping("/verify-email")
     public ModelAndView verifyEmail(@RequestParam(required = false) final String candidate, @RequestParam final String email,
-                                    final Principal principal, final HttpServletRequest request) {
+                                    final Principal principal, final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException, ServletException {
         final var username = principal.getName();
+
+        if (StringUtils.isEmpty(candidate)) {
+            return verifyEmailRequest(principal, request, response, "noselection");
+        }
 
         final var chosenEmail = StringUtils.trim(StringUtils.isBlank(candidate) || "other".equals(candidate) ? email : candidate);
 
