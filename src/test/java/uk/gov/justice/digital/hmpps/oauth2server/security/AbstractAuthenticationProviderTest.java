630f573d1e6b1e3907cf31f547f263c91a6d79cb
Peter Phillips <peter@greenthistle.co.uk>
2019-01-29 08:21:27 +0000

    NN-1545: and another one

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/security/AbstractAuthenticationProviderTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/security/AbstractAuthenticationProviderTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@ActiveProfiles({"dev"})
+@ActiveProfiles("dev-seed")
 public class AbstractAuthenticationProviderTest {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
