af5e88fab8e5ebe52de99d4982e309ebdf9196c7
Peter Phillips <peter@greenthistle.co.uk>
2019-01-30 15:20:22 +0000

    NN-1545: Get sample data working against sqlserver too

--- src/main/resources/db/dev/data/auth/V9_1__oauth_sample_data.sql
+++ src/main/resources/db/dev/data/auth/V9_1__oauth_sample_data.sql
@@ -27,8 +27,8 @@ VALUES ('LOCKED_USER', 'locked@somewhere.com', 'true'),
 
 INSERT INTO user_email (username, password, email, verified, enabled, master)
 VALUES ('AUTH_ONLY_USER', '{bcrypt}$2a$10$Fmcp2KUKRW53US3EJfsxkOh.ekZhqz5.Baheb9E98QLwEFLb9csxy',
-        'auth_only_user@digital.justice.gov.uk', true, true, true),
-('AUTH_ONLY_TEST', '{bcrypt}$2a$10$Fmcp2KUKRW53US3EJfsxkOh.ekZhqz5.Baheb9E98QLwEFLb9csxy', 'auth_only_test@digital.justice.gov.uk', true, true, true);
+        'auth_only_user@digital.justice.gov.uk', 'true', 'true', 'true'),
+('AUTH_ONLY_TEST', '{bcrypt}$2a$10$Fmcp2KUKRW53US3EJfsxkOh.ekZhqz5.Baheb9E98QLwEFLb9csxy', 'auth_only_test@digital.justice.gov.uk', 'true', 'true', 'true');
 
 INSERT INTO user_token (token, token_type, token_expiry, username)
 VALUES ('reset', 'RESET', '2018-12-10 08:55:45', 'LOCKED_USER');
