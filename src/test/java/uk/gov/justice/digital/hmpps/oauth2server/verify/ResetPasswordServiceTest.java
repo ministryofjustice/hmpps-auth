2a8cd717d314fb5891b280904cc8fe0c7b5e0d9f
Peter Phillips <peter@greenthistle.co.uk>
2019-02-18 19:17:42 +0000

    NN-1681: Add in password expiry for nomis users by comparing expiry_date column

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/verify/ResetPasswordServiceTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/verify/ResetPasswordServiceTest.java
@@ -212,7 +212,7 @@ public class ResetPasswordServiceTest {
         staff.setFirstName("bOb");
         staff.setStatus("ACTIVE");
         staffUserAccount.setStaff(staff);
-        final var detail = new AccountDetail("user", "OPEN", "profile");
+        final var detail = new AccountDetail("user", "OPEN", "profile", null);
         staffUserAccount.setAccountDetail(detail);
         return Optional.of(staffUserAccount);
     }
