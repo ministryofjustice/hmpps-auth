0117188cef311c825809826fe64038f0410e3959
Peter Phillips <peter@greenthistle.co.uk>
2019-03-28 13:12:17 +0000

    NN-1739: Improvements after code review

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/auth/model/Authority.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/auth/model/Authority.java
@@ -16,6 +16,8 @@ import java.util.UUID;
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"authority"})
 public class Authority implements GrantedAuthority {
+    private static final String ROLE_PREFIX = "ROLE_";
+
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
@@ -31,10 +33,10 @@ public class Authority implements GrantedAuthority {
 
     public String getAuthorityName() {
         // strip off ROLE_
-        return authority.substring(5);
+        return authority.substring(ROLE_PREFIX.length());
     }
 
     public static String addRolePrefixIfNecessary(final String role) {
-        return StringUtils.startsWith(role, "ROLE_") ? role : String.format("ROLE_%s", role);
+        return StringUtils.startsWith(role, ROLE_PREFIX) ? role : ROLE_PREFIX + role;
     }
 }
