546f8a66c89f929c34f1fddfc971251c73e7a836
Peter Phillips <peter@greenthistle.co.uk>
2019-08-07 16:18:27 +0100

    DT-141: Fix user email description

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/model/EmailAddress.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/model/EmailAddress.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import uk.gov.justice.digital.hmpps.oauth2server.auth.model.UserEmail;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@ApiModel(description = "User Details")
+@ApiModel(description = "User email details")
 @Data
-@Builder
 @AllArgsConstructor
 public class EmailAddress {
     @ApiModelProperty(required = true, value = "Username", example = "DEMO_USER1", position = 1)
@@ -21,6 +19,6 @@ public class EmailAddress {
     private final String email;
 
     public static EmailAddress fromUserEmail(final UserEmail u) {
-        return builder().username(u.getUsername()).email(u.getEmail()).build();
+        return new EmailAddress(u.getUsername(), u.getEmail());
     }
 }
