fa27c1444cada60eededf2e7fd246498b861df87
Peter Phillips <peter@greenthistle.co.uk>
2019-08-14 12:57:16 +0100

    DT-153: Move to separate flyway scripts as wasn't working in sqlserver

---
+++ src/main/resources/db/auth/V2_8__oauth_service_update_email.sql
@@ -0,0 +1,19 @@
+update oauth_service
+set email = 'hdcdigitalservice@digital.justice.gov.uk'
+where code = 'HDC';
+
+update oauth_service
+set email = 'feedback@digital.justice.gov.uk'
+where code = 'NOMIS';
+
+update oauth_service
+set email = 'categorisation@justice.gov.uk'
+where code = 'CATTOOL';
+
+update oauth_service
+set email = 'bookasecuremove@digital.justice.gov.uk'
+where code = 'BOOK_MOVE';
+
+update oauth_service
+set email = 'moic@digital.justice.gov.uk'
+where code = 'POM';
