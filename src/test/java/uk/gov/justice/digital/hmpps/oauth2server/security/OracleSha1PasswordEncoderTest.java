ac71f17c6b85220dada6ead7389ffe45a0a5bb6d
petergphillips <peter@greenthistle.co.uk>
2019-09-06 14:39:22 +0100

    DT-193: Fix users without password null pointer (#250)

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/security/OracleSha1PasswordEncoderTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/security/OracleSha1PasswordEncoderTest.java
@@ -6,14 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OracleSha1PasswordEncoderTest {
     @Test
-    public void testMatchesOraclePassword() {
+    public void matches_testMatchesOraclePassword() {
         final var encodedPassword = "S:39BA463D55E5C8936A6798CC37B1347BA8BEC37B6407397EB769BC356F0C";
         assertThat(new OracleSha1PasswordEncoder().matches("somepass1", encodedPassword)).isTrue();
     }
 
     @Test
-    public void invalidPasswordLength() {
+    public void matches_invalidPasswordLength() {
         final var encodedPassword = "somepasswordthatshouldbeencoded";
         assertThat(new OracleSha1PasswordEncoder().matches("somepass1", encodedPassword)).isFalse();
+    }
+
+    @Test
+    public void matches_nullInput() {
+        assertThat(new OracleSha1PasswordEncoder().matches("somepass1", null)).isFalse();
     }
 }
