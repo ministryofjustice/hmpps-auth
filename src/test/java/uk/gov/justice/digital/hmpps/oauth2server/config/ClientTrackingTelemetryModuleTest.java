a0af8c8230d799b5d3782dea7a8490e6281163b2
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2019-10-17 14:51:24 +0100

    DT-292: added telemetryModule to add client_id and user_name as custo… (#274)
    * DT-292: added telemetryModule to add client_id and user_name as customDimension to app insights
    
    * DT-292: added lombok as testsCompileOnly and testAnnotationProcessors to gradle file
    
    * DT-292: updated lombok version for tests and added tearDown to ClientTrackingTelemetryModuleTest

---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/config/ClientTrackingTelemetryModuleTest.java
@@ -0,0 +1,92 @@
+package uk.gov.justice.digital.hmpps.oauth2server.config;
+
+import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
+import com.microsoft.applicationinsights.web.internal.ThreadContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.justice.digital.hmpps.oauth2server.utils.JwtAuthHelper;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.justice.digital.hmpps.oauth2server.utils.JwtAuthHelper.JwtParameters;
+
+@RunWith(SpringRunner.class)
+@Import({JwtAuthHelper.class, ClientTrackingTelemetryModule.class})
+@ContextConfiguration(initializers = {ConfigFileApplicationContextInitializer.class})
+@ActiveProfiles("test")
+public class ClientTrackingTelemetryModuleTest {
+
+    @Autowired
+    private ClientTrackingTelemetryModule clientTrackingTelemetryModule;
+
+    @Autowired
+    private JwtAuthHelper jwtAuthHelper;
+
+    @Before
+    public void setup() {
+        ThreadContext.setRequestTelemetryContext(new RequestTelemetryContext(1L));
+    }
+
+    @After
+    public void tearDown() {
+        ThreadContext.remove();
+    }
+
+    @Test
+    public void shouldAddClientIdAndUserNameToInsightTelemetry() {
+
+        final var token = createJwt("bob", List.of(), 1L);
+
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        clientTrackingTelemetryModule.onBeginRequest(req, res);
+
+        final var insightTelemetry = ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry().getProperties();
+
+        assertThat(insightTelemetry).hasSize(2);
+        assertThat(insightTelemetry.get("user_name")).isEqualTo("bob");
+        assertThat(insightTelemetry.get("client_id")).isEqualTo("elite2apiclient");
+
+    }
+
+    @Test
+    public void shouldNotAddClientIdAndUserNameToInsightTelemetryAsTokenExpired() {
+
+        final var token = createJwt("Fred", List.of(), -1L);
+
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        clientTrackingTelemetryModule.onBeginRequest(req, res);
+
+        final var insightTelemetry = ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry().getProperties();
+
+        assertThat(insightTelemetry).isEmpty();
+    }
+
+    private String createJwt(final String user, final List<String> roles, Long duration) {
+        return jwtAuthHelper.createJwt(JwtParameters.builder()
+                .username(user)
+                .roles(roles)
+                .scope(List.of("read", "write"))
+                .expiryTime(Duration.ofDays(duration))
+                .build());
+    }
+
+}
