fa3f49d23e5dac22abf5f78ee27f1ff63cacc231
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-01-21 09:24:46 +0000

    DT-494: converted verifyEmailService tests to Kotlin

---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/verify/VerifyEmailServiceIntTest.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.oauth2server.verify
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Transactional
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("dev")
+@Transactional(transactionManager = "authTransactionManager")
+open class VerifyEmailServiceIntTest {
+  @Autowired
+  private val jdbcTemplate: JdbcTemplate? = null
+  private lateinit var verifyEmailService: VerifyEmailService
+  @Before
+  fun setUp() {
+    verifyEmailService = VerifyEmailService(null, null, jdbcTemplate, null, null, null, "templateId")
+  }
+
+  @Test
+  fun existingEmailAddresses() {
+      val emails = verifyEmailService.getExistingEmailAddresses("RO_USER")
+      assertThat(emails).containsExactlyInAnyOrder("phillips@bobjustice.gov.uk", "phillips@fredjustice.gov.uk")
+    }
+
+  @Test
+  fun existingEmailAddresses_NotFound() {
+      val emails = verifyEmailService.getExistingEmailAddresses("CA_USER")
+      assertThat(emails).isEmpty()
+    }
+}
