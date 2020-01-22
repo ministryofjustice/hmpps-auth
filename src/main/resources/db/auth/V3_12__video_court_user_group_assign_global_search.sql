4c5e9881492cb31a9febc15bc37283b6e68b39e7
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2020-01-22 09:39:19 +0000

    NN-2394: Add GLOBAL_SEARCH as a default role to the VIDEO_LINK_COURT_USER group

---
+++ src/main/resources/db/auth/V3_12__video_court_user_group_assign_global_search.sql
@@ -0,0 +1,5 @@
+INSERT INTO group_assignable_role (role_id, group_id, automatic)
+SELECT role_id, group_id, 'true'
+FROM groups g, roles r
+WHERE r.role_code = 'GLOBAL_SEARCH'
+  AND g.group_code = 'VIDEO_LINK_COURT_USER';
