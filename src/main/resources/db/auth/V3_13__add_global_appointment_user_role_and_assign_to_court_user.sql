baa9278dad08860043314dc0e65a2b7992323362
JamieDraperUK <draper65@googlemail.com>
2020-01-22 15:33:45 +0000

    NN-2392 rename role

--- src/main/resources/db/auth/V3_13__add_global_appointment_user_role_and_assign_to_court_user.sql
+++ src/main/resources/db/auth/V3_13__add_global_appointment_user_role_and_assign_to_court_user.sql
@@ -1,8 +1,8 @@
 INSERT INTO roles (role_id, role_code, role_name)
-VALUES (newid(), 'EXTERNAL_APPOINTMENT', 'External appointment');
+VALUES (newid(), 'GLOBAL_APPOINTMENT', 'External appointment');
 
 INSERT INTO group_assignable_role (role_id, group_id, automatic)
 SELECT role_id, group_id, 'true'
 FROM groups g, roles r
-WHERE r.role_code = 'EXTERNAL_APPOINTMENT'
+WHERE r.role_code = 'GLOBAL_APPOINTMENT'
   AND g.group_code = 'VIDEO_LINK_COURT_USER';
