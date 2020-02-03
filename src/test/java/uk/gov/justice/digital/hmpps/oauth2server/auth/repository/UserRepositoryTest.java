7cf6748e6ac5fa98fef1aa4025472ed4a9c6228d
Peter Phillips <peter@greenthistle.co.uk>
2020-02-03 15:05:13 +0000

    DT-517: Fix flaky test

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/auth/repository/UserRepositoryTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/auth/repository/UserRepositoryTest.java
@@ -270,7 +270,7 @@ public class UserRepositoryTest {
     public void findAll_UserFilter_ByEmail() {
         assertThat(repository.findAll(UserFilter.builder().name("test@digital").build()))
                 .extracting(User::getUsername)
-                .containsOnly("AUTH_TEST", "AUTH_RO_USER_TEST");
+                .containsOnly("AUTH_TEST", "AUTH_RO_USER_TEST", "AUTH_CHANGE_TEST", "AUTH_CHANGE2_TEST");
     }
 
     @Test
