e58be56505b88cb4e2abfcbe92cf0b133979fb56
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-02-21 17:52:34 +0000

    DT-370 Publish JWK set containing public key for consuming by clients

---
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/resource/api/JwkSetRestController.java
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.oauth2server.resource.api;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class JwkSetRestController {
+
+    private JWKSet jwkSet;
+
+    public JwkSetRestController(@Autowired final JWKSet jwkSet) {
+        this.jwkSet = jwkSet;
+    }
+
+    @GetMapping("/.well-known/jwks.json")
+    public Map<String, Object> keys() {
+        return this.jwkSet.toJSONObject();
+    }
+
+}
