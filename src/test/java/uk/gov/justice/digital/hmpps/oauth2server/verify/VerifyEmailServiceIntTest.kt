1f0530818328fb24413989800ad47874c194060e
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-02-28 12:45:59 +0000

    DT-370 Upgrade dependencies and fix build warnings

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/verify/VerifyEmailServiceIntTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/verify/VerifyEmailServiceIntTest.kt
@@ -2,8 +2,6 @@ package uk.gov.justice.digital.hmpps.oauth2server.verify
 
 import com.microsoft.applicationinsights.TelemetryClient
 import com.nhaarman.mockito_kotlin.mock
-import junit.framework.Assert.assertFalse
-import junit.framework.Assert.assertTrue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -49,9 +47,9 @@ open class VerifyEmailServiceIntTest {
   @Test
   fun emailAddressSetToNotVerified() {
     val userBefore = userRepository.findByUsername("AUTH_CHANGE_EMAIL")
-    assertTrue(userBefore.get().isVerified)
+    assertThat(userBefore.get().isVerified).isTrue()
     verifyEmailService.requestVerification("AUTH_CHANGE_EMAIL", "phillips@fredjustice.gov.uk", "AUTH", "url")
     val userAfter = userRepository.findByUsername("AUTH_CHANGE_EMAIL")
-    assertFalse(userAfter.get().isVerified)
+    assertThat(userAfter.get().isVerified).isFalse()
   }
 }
