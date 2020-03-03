5db4c6018e29df9bb434d5164dcbd96f56070598
Peter Phillips <peter@greenthistle.co.uk>
2020-03-03 07:28:34 +0000

    DT-569: More int tests from gradle

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/IntegrationTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/IntegrationTest.kt
@@ -2,18 +2,34 @@ package uk.gov.justice.digital.hmpps.oauth2server.resource
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.oauth2server.utils.JwtAuthHelper
+import java.time.Duration
+
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("dev")
+@Import(JwtAuthHelper::class)
 abstract class IntegrationTest {
   @Autowired
   lateinit var webTestClient: WebTestClient
+
+  @Autowired
+  private lateinit var jwtAuthHelper: JwtAuthHelper
 
   init {
     // Resolves an issue where Wiremock keeps previous sockets open from other tests causing connection resets
     System.setProperty("http.keepAlive", "false")
   }
+
+  internal fun setAuthorisation(user: String): (org.springframework.http.HttpHeaders) -> Unit {
+    val token = createJwt(user)
+    return { it.set(org.springframework.http.HttpHeaders.AUTHORIZATION, "Bearer $token") }
+  }
+
+  private fun createJwt(user: String) =
+      jwtAuthHelper.createJwt(JwtAuthHelper.JwtParameters(username = user, scope = listOf("read", "write"), expiryTime = Duration.ofHours(1L)))
 }
---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/api/AuthAllGroupsIntTest.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.oauth2server.resource.api
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.oauth2server.resource.IntegrationTest
+
+class AuthAllGroupsIntTest : IntegrationTest() {
+  @Test
+  fun `Auth Groups endpoint returns all possible auth groups`() {
+    webTestClient
+        .get().uri("/auth/api/authgroups")
+        .headers(setAuthorisation("AUTH_ADM"))
+        .exchange()
+        .expectStatus().isOk
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .jsonPath(".[?(@.groupCode == 'SITE_1_GROUP_1')]").isEqualTo(mapOf("groupCode" to "SITE_1_GROUP_1", "groupName" to "Site 1 - Group 1"))
+        .jsonPath(".[*].groupCode").value<List<String>> {
+          assertThat(it).hasSizeGreaterThan(2)
+        }
+  }
+
+  @Test
+  fun `Auth Groups endpoint not accessible without valid token`() {
+    webTestClient.get().uri("/auth/api/authgroups")
+        .exchange()
+        .expectStatus().isUnauthorized
+  }
+}
