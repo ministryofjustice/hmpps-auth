d799b1fa665d800d00c74c7bce2d69f648c8d423
Peter Phillips <peter@greenthistle.co.uk>
2020-03-05 14:41:23 +0000

    DT-524: Change verify email specification to fluentlenium

---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/integration/VerifyEmailSpecification.kt
@@ -0,0 +1,174 @@
+package uk.gov.justice.digital.hmpps.oauth2server.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.fluentlenium.core.annotation.Page
+import org.fluentlenium.core.annotation.PageUrl
+import org.fluentlenium.core.domain.FluentWebElement
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.support.FindBy
+
+class VerifyEmailSpecification : AbstractAuthSpecification() {
+  @Page
+  private lateinit var verifyEmailPage: VerifyEmailPage
+
+  @Page
+  private lateinit var verifyEmailSentPage: VerifyEmailSentPage
+
+  @Page
+  private lateinit var verifyEmailConfirmPage: VerifyEmailConfirmPage
+
+  @Test
+  fun `A user can cancel email verification`() {
+    goTo(loginPage).loginAsUnverifiedEmail("DM_USER", "password123456")
+    verifyEmailPage.isAt()
+
+    verifyEmailPage.cancel()
+    newInstance(HomePage::class.java).isAt()
+  }
+
+  @Test
+  fun `A user is not allowed to verify a gsi email address`() {
+    goTo(loginPage).loginAsUnverifiedEmail("AUTH_NO_EMAIL", "password123456")
+    verifyEmailPage.isAt()
+
+    verifyEmailPage.verifyEmailAs("dm_user@hmps.gsi.gov.uk")
+
+    verifyEmailPage.isAtError()
+    assertThat(verifyEmailPage.getErrorDetail()).startsWith("All gsi.gov.uk have now been migrated to a justice.gov.uk domain. Enter your justice.gov.uk address instead.")
+  }
+
+  @Test
+  fun `A user can verify a previously chosen email`() {
+    goTo(loginPage).loginAsUnverifiedEmail("DM_USER", "password123456")
+
+    verifyEmailPage.isAt()
+
+    verifyEmailPage.verifyExistingEmailAs("dm_user@digital.justice.gov.uk")
+
+    verifyEmailSentPage.isAt()
+    val verifyLink = verifyEmailSentPage.getVerifyLink()
+    verifyEmailSentPage.continueProcess()
+
+    newInstance(HomePage::class.java).isAt()
+
+    goTo(verifyLink)
+    verifyEmailConfirmPage.isAt()
+
+    goTo(verifyLink)
+    verifyEmailConfirmPage.isAt()
+  }
+
+  @Test
+  fun `A user can verify an email that exists in pnomis`() {
+    goTo(loginPage).loginAsUnverifiedEmail("RO_USER", "password123456")
+
+    verifyEmailPage.isAt()
+
+    verifyEmailPage.selectExistingEmailAs("phillips@bobjustice.gov.uk")
+
+    verifyEmailSentPage.isAt()
+    val verifyLink = verifyEmailSentPage.getVerifyLink()
+    verifyEmailSentPage.continueProcess()
+
+    newInstance(HomePage::class.java).isAt()
+
+    goTo(verifyLink)
+    verifyEmailConfirmPage.isAt()
+  }
+
+  @Test
+  fun `A user can verify an email that exists in pnomis where the user has changed password in auth`() {
+    goTo(loginPage).loginAsUnverifiedEmail("RO_DEMO", "password123456")
+
+    verifyEmailPage.isAt()
+
+    verifyEmailPage.selectExistingEmailAs("ro_user@some.justice.gov.uk")
+
+    verifyEmailSentPage.isAt()
+    val verifyLink = verifyEmailSentPage.getVerifyLink()
+    verifyEmailSentPage.continueProcess()
+
+    newInstance(HomePage::class.java).isAt()
+
+    goTo(verifyLink)
+    verifyEmailConfirmPage.isAt()
+  }
+
+  @Test
+  fun `An auth only user can verify their email address`() {
+    goTo(loginPage).loginAsUnverifiedEmail("AUTH_NO_EMAIL", "password123456")
+
+    verifyEmailPage.isAt()
+
+    verifyEmailPage.verifyEmailAs("auth_no_email@digital.justice.gov.uk")
+
+    verifyEmailSentPage.isAt()
+    val verifyLink = verifyEmailSentPage.getVerifyLink()
+    verifyEmailSentPage.continueProcess()
+
+    newInstance(HomePage::class.java).isAt()
+
+    goTo(verifyLink)
+    verifyEmailConfirmPage.isAt()
+  }
+
+  @Test
+  fun `A user is asked to sign in again if the verification link is invalid`() {
+    goTo("/verify-email-confirm?token=someinvalidtoken")
+    val verifyEmailConfirmErrorPage = newInstance(VerifyEmailConfirmErrorPage::class.java)
+    verifyEmailConfirmErrorPage.isAtError()
+    assertThat(verifyEmailConfirmErrorPage.getErrorDetail()).startsWith("This link is invalid")
+  }
+}
+
+@PageUrl("/verify-email")
+open class VerifyEmailPage : AuthPage("HMPPS Digital Services - Verify Email", "Verify your email address") {
+  @FindBy(css = "input[type='submit']")
+  private lateinit var verifyEmailButton: FluentWebElement
+
+  @FindBy(css = "#email")
+  private lateinit var emailInput: FluentWebElement
+
+  fun verifyEmailAs(email: String) {
+    emailInput.fill().withText(email)
+    assertThat(verifyEmailButton.value()).isEqualTo("Save")
+    verifyEmailButton.click()
+  }
+
+  fun verifyExistingEmailAs(email: String) {
+    assertThat(emailInput.value()).isEqualTo(email)
+    assertThat(verifyEmailButton.value()).isEqualTo("Resend email")
+    verifyEmailButton.click()
+  }
+
+  fun selectExistingEmailAs(email: String) {
+    el("input[value='$email']").click()
+    assertThat(verifyEmailButton.value()).isEqualTo("Save")
+    verifyEmailButton.click()
+  }
+
+  fun cancel() {
+    val cancelButton = el("a[id='cancel']")
+    assertThat(cancelButton.text()).isEqualTo("Skip for now")
+    cancelButton.click()
+  }
+}
+
+@PageUrl("/verify-email")
+open class VerifyEmailSentPage : AuthPage("HMPPS Digital Services - Verify Email Sent", "Verification email sent") {
+  @FindBy(css = "a[role='button']")
+  private lateinit var continueButton: FluentWebElement
+
+  fun continueProcess() {
+    assertThat(continueButton.text()).isEqualTo("Continue")
+    continueButton.click()
+  }
+
+  fun getVerifyLink() = el("#verifyLink").attribute("href")
+}
+
+@PageUrl("/verify-email-confirm")
+open class VerifyEmailConfirmPage : AuthPage("HMPPS Digital Services - Verify Email Confirmation", "Email address verified")
+
+@PageUrl("/verify-email-confirm")
+open class VerifyEmailConfirmErrorPage : AuthPage("HMPPS Digital Services - Verify Email Confirmation", "Unable to confirm email")
