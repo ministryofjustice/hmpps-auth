eedf89806b71038928cea2153fe9ab618f6a3d5a
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-03-23 14:35:07 +0000

    DT-665 Indentation corrected.

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/config/JWTTokenEnhancerTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/config/JWTTokenEnhancerTest.kt
@@ -17,33 +17,33 @@ import uk.gov.justice.digital.hmpps.oauth2server.security.UserDetailsImpl
 import java.util.*
 
 internal class JWTTokenEnhancerTest {
-    private val authentication: OAuth2Authentication = mock()
+  private val authentication: OAuth2Authentication = mock()
 
-    @Test
-    fun testEnhance_HasUserToken() {
-        val token: OAuth2AccessToken = DefaultOAuth2AccessToken("value")
-        whenever(authentication.isClientOnly).thenReturn(false)
-        val uuid = UUID.randomUUID()
-        val user = User.builder().id(uuid).username("user").source(AuthSource.auth).build()
-        whenever(authentication.userAuthentication).thenReturn(UsernamePasswordAuthenticationToken(user, "pass"))
-        JWTTokenEnhancer().enhance(token, authentication)
-        assertThat(token.additionalInformation).containsOnly(
-                entry("sub", "user"),
-                entry("user_name", "user"),
-                entry("auth_source", "auth"),
-                entry("user_id", uuid.toString()))
-    }
+  @Test
+  fun testEnhance_HasUserToken() {
+    val token: OAuth2AccessToken = DefaultOAuth2AccessToken("value")
+    whenever(authentication.isClientOnly).thenReturn(false)
+    val uuid = UUID.randomUUID()
+    val user = User.builder().id(uuid).username("user").source(AuthSource.auth).build()
+    whenever(authentication.userAuthentication).thenReturn(UsernamePasswordAuthenticationToken(user, "pass"))
+    JWTTokenEnhancer().enhance(token, authentication)
+    assertThat(token.additionalInformation).containsOnly(
+        entry("sub", "user"),
+        entry("user_name", "user"),
+        entry("auth_source", "auth"),
+        entry("user_id", uuid.toString()))
+  }
 
-    @Test
-    fun testEnhance_MissingAuthSource() {
-        val token: OAuth2AccessToken = DefaultOAuth2AccessToken("value")
-        whenever(authentication.isClientOnly).thenReturn(false)
-        whenever(authentication.userAuthentication).thenReturn(UsernamePasswordAuthenticationToken(UserDetailsImpl("user", null, emptyList(), null, "userID"), "pass"))
-        JWTTokenEnhancer().enhance(token, authentication)
-        assertThat(token.additionalInformation).containsOnly(
-                entry("sub", "user"),
-                entry("user_name", "user"),
-                entry("auth_source", "none"),
-                entry("user_id", "userID"))
-    }
+  @Test
+  fun testEnhance_MissingAuthSource() {
+    val token: OAuth2AccessToken = DefaultOAuth2AccessToken("value")
+    whenever(authentication.isClientOnly).thenReturn(false)
+    whenever(authentication.userAuthentication).thenReturn(UsernamePasswordAuthenticationToken(UserDetailsImpl("user", null, emptyList(), null, "userID"), "pass"))
+    JWTTokenEnhancer().enhance(token, authentication)
+    assertThat(token.additionalInformation).containsOnly(
+        entry("sub", "user"),
+        entry("user_name", "user"),
+        entry("auth_source", "none"),
+        entry("user_id", "userID"))
+  }
 }
