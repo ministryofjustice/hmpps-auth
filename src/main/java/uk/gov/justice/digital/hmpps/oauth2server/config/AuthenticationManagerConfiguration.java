d02be9cb6f4bc44c6b86c1c37ad268170f787286
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-03-26 11:23:03 +0000

    DT-629: added url for verifying secondary email address to allowed urls

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/AuthenticationManagerConfiguration.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/AuthenticationManagerConfiguration.java
@@ -129,7 +129,7 @@ public class AuthenticationManagerConfiguration extends WebSecurityConfigurerAda
                 .ignoring()
                 .antMatchers("/css/**", "/js/**", "/images/**", "/fonts/**", "/webjars/**", "/favicon.ico",
                         "/health", "/health/ping", "/info", "/ping", "/error", "/terms", "/contact", "/change-password",
-                        "/verify-email-confirm", "/forgot-password", "/reset-password", "/set-password",
+                        "/verify-email-confirm", "/verify-email-secondary-confirm", "/forgot-password", "/reset-password", "/set-password",
                         "/reset-password-confirm", "/reset-password-success", "/reset-password-select",
                         "/initial-password", "/initial-password-success", "/mfa-challenge", "/mfa-resend",
                         "/h2-console/**", "/v2/api-docs", "/jwt-public-key",
