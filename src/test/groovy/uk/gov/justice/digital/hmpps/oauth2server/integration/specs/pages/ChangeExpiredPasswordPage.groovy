3802895c3ca3f578ec425d0ea506adf7de692a3b
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-04-23 10:13:07 +0100

    DT-740: update save password button wording

--- src/test/groovy/uk/gov/justice/digital/hmpps/oauth2server/integration/specs/pages/ChangeExpiredPasswordPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/oauth2server/integration/specs/pages/ChangeExpiredPasswordPage.groovy
@@ -26,7 +26,7 @@ class ChangeExpiredPasswordPage extends Page {
     $('form').newPassword = newPassword
     $('form').confirmPassword = confirmNewPassword
 
-    assert changePasswordButton.value() == 'Save new password'
+    assert changePasswordButton.value() == 'Save password'
 
     changePasswordButton.click()
   }
