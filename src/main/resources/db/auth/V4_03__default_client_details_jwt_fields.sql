e75106620a99ac8896e744743edf3003630b4a8b
Peter Phillips <peter@greenthistle.co.uk>
2020-06-10 10:14:29 +0100

    DT-896: Also cope with null for additional information

--- src/main/resources/db/auth/V4_03__default_client_details_jwt_fields.sql
+++ src/main/resources/db/auth/V4_03__default_client_details_jwt_fields.sql
@@ -1,6 +1,6 @@
 UPDATE oauth_client_details
 SET additional_information = '{"jwtFields":"-name,+user_name"}'
-WHERE additional_information = '{}';
+WHERE additional_information = '{}' or additional_information is null;
 
 UPDATE oauth_client_details
 SET additional_information = '{}'
