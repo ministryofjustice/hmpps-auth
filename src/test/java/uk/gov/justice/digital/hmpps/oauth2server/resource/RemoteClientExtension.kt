b476740d1129468a46dbb3f9adb797c2d90c3be2
petergphillips <peter@greenthistle.co.uk>
2020-09-02 13:01:13 +0100

    DT-1085: 🔧 Add PECS Supplier to list of 2fa accounts (#526)
    * DT-1085: 🔧 Add PECS Supplier to list of 2fa accounts
    
    * DT-1085: 👽 Cope with chromedriver 85 update that fails for connection refused

---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/RemoteClientExtension.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.hmpps.oauth2server.resource
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+class RemoteClientExtension : BeforeAllCallback, AfterAllCallback, BeforeEachCallback {
+  companion object {
+    @JvmField
+    val remoteClient = RemoteClientMockServer()
+  }
+
+  override fun beforeAll(context: ExtensionContext) {
+    remoteClient.start()
+  }
+
+  override fun beforeEach(context: ExtensionContext) {
+    remoteClient.resetRequests()
+  }
+
+  override fun afterAll(context: ExtensionContext) {
+    remoteClient.stop()
+  }
+}
+
+class RemoteClientMockServer : WireMockServer(wireMockConfig().port(8081).usingFilesUnderClasspath("remoteClient"))
