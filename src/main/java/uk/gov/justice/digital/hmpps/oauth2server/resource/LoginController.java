4058be6a354a36639c3f0ad86970950f3ef47fe8
Ed Brindley <ed@maidavale.org>
2020-09-24 14:35:00 +0100

    AC-311 - Add links to login page for authenticating via OAuth2 clients, if configured (#551)
    Co-authored-by: petergphillips <peter@greenthistle.co.uk>

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/resource/LoginController.java
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/resource/LoginController.java
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.oauth2server.resource;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,26 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class LoginController {
+    final private List<ClientRegistration> clientRegistrations;
+
+    public LoginController(final Optional<InMemoryClientRegistrationRepository> clientRegistrationRepository) {
+        clientRegistrations = clientRegistrationRepository.map(registrations -> StreamSupport
+                .stream(registrations.spliterator(), false)
+                .collect(Collectors.toList()))
+                .orElse(Collections.emptyList());
+    }
+
     @GetMapping("/login")
     public ModelAndView loginPage(@RequestParam(required = false) final String error) {
-        final var modelAndView = new ModelAndView("login");
+        final var modelAndView = new ModelAndView("login", Collections.singletonMap("oauth2Clients", clientRegistrations));
         // send bad request if password wrong so that browser won't offer to save the password
         if (StringUtils.isNotBlank(error)) {
             modelAndView.setStatus(HttpStatus.BAD_REQUEST);
--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/LoginControllerTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/LoginControllerTest.kt
@@ -3,14 +3,21 @@ package uk.gov.justice.digital.hmpps.oauth2server.resource
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import java.util.*
 
 class LoginControllerTest {
-  private val controller = LoginController()
+  private val clientRegistrationRepository: Optional<InMemoryClientRegistrationRepository> = Optional.empty()
+
+  private val controller = LoginController(clientRegistrationRepository)
   @Test
   fun loginPage_NoError() {
     val modelAndView = controller.loginPage(null)
     assertThat(modelAndView.viewName).isEqualTo("login")
     assertThat(modelAndView.status).isNull()
+    assertThat(modelAndView.modelMap["oauth2Clients"]).asList().isEmpty()
   }
 
   @Test
@@ -18,5 +25,28 @@ class LoginControllerTest {
     val modelAndView = controller.loginPage("bad")
     assertThat(modelAndView.viewName).isEqualTo("login")
     assertThat(modelAndView.status).isEqualTo(HttpStatus.BAD_REQUEST)
+    assertThat(modelAndView.modelMap["oauth2Clients"]).asList().isEmpty()
+  }
+
+  @Test
+  fun `login page shows links when OIDC clients are configured`() {
+    val clients = listOf(ClientRegistration
+        .withRegistrationId("test")
+        .clientName("Test")
+        .clientId("bd4de96a-437d-4fef-b1b2-5f4c1e39c080")
+        .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+        .authorizationUri("/test")
+        .tokenUri("/test")
+        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+        .build())
+
+    val clientRegistrationRepository = Optional.of(InMemoryClientRegistrationRepository(clients))
+
+    val controller = LoginController(clientRegistrationRepository)
+
+    val modelAndView = controller.loginPage(null)
+    assertThat(modelAndView.viewName).isEqualTo("login")
+    assertThat(modelAndView.status).isNull()
+    assertThat(modelAndView.modelMap["oauth2Clients"]).asList().hasSize(1)
   }
 }
