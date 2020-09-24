fface2630492a41ec94f11156feaebc83a2fd696
Ed Brindley <ed@maidavale.org>
2020-09-24 09:27:10 +0100

    AC-311 - Fix Username column width change to run on SQL Server. Also replace invalid UUIDs in seed data with correct ones (#549)

--- src/main/resources/db/auth/V4_11__increase_username_length.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE user_retries ALTER COLUMN username varchar(37);
-ALTER TABLE users ALTER COLUMN username varchar(37);
---
+++ src/main/resources/db/auth_h2/V4_11__increase_username_length.sql
@@ -0,0 +1,2 @@
+ALTER TABLE user_retries ALTER COLUMN username varchar(37) NOT NULL;
+ALTER TABLE users ALTER COLUMN username varchar(37) NOT NULL;
