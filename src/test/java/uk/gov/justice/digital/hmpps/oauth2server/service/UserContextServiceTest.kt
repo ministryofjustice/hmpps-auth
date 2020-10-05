d700dad5144ef833e9bfda2df68f0d22edbdd8ed
Tom Myers <63233073+tomsmyers@users.noreply.github.com>
2020-10-05 11:45:48 +0100

    AC-312 add AzureAD -> nDelius user mapping  (#559)
    * add new 'delius_id' field to token for delius logins if the client has ROLE_ACCESS_DELIUS_ID
    
    * add delius id into /api/user/me response if present in access token
    
    * add client for testing and fix string equality check
    
    * tidy up; log error on parse failure
    
    * fix existing tests
    
    * change field from delius_id -> delius_username
    
    * poc user mapping from azure/auth users
    
    * remove any 'active' code to leave just the skeleton user mapping class
    
    * clean up modified code
    
    * Revert "fix existing tests"
    
    This reverts commit a05ba4228413211e09cfae0f1a6bd315fb6baebe.
    
    * formatting, and return null if 'to' == 'from'
    
    * refactoring based on review comments
    
    * re-add code removed in error
    
    * fix unwanted behaviour change in authSource
    
    * make 'map' method in UserContextService private
    
    * fix existing tests
    
    * fix formatting and ensure all tests are passing
    
    * added unit tests and some mild refactoring
    
    * style issues in kotlin test
    
    * AC-15: Integrate Azure AD with HMPPS Auth
    
    Amend imports
    
    Update MSAL library
    
    Rough implementation of Azure AD Auth code flow
    
    Remove dependency on Microsoft libs, go for naive azure user service for the time being
    
    Further fixes
    
    Fix config
    
    Change to using preferred_username field rather than UPN, add flag to disable Azure link
    
    * add azure -> delius user mapping
    
    * fix unit tests
    
    * add username to delius user response in integration tests
    
    * AC-15: Integrate Azure AD with HMPPS Auth
    
    Amend imports
    
    Update MSAL library
    
    Rough implementation of Azure AD Auth code flow
    
    Remove dependency on Microsoft libs, go for naive azure user service for the time being
    
    Further fixes
    
    Fix config
    
    Change to using preferred_username field rather than UPN, add flag to disable Azure link
    
    * AC-311 - Add integration test for Azure OIDC login
    
    * fix merge
    
    * Revert "AC-311 - Add integration test for Azure OIDC login"
    
    This reverts commit 4e0487b0fd9fb83e1589b04b6409a63596b1431d.
    
    * DT-1072: 💩✨ Proof of concept of requesting user account during authorize flow
    
    * add unit tests for getDeliusUserByEmail
    
    * address review comments in UserContextService
    
    * other small review comment fixes
    
    * Revert "DT-1072: 💩✨ Proof of concept of requesting user account during authorize flow"
    
    This reverts commit bd9108e4144c7b18c434479a4b9d1a48623573f3.
    
    * capitalize log messages
    
    Co-authored-by: ed <ed@maidavale.org>
    Co-authored-by: Peter Phillips <peter@greenthistle.co.uk>

---
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/service/UserContextServiceTest.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.oauth2server.service
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.anyString
+import uk.gov.justice.digital.hmpps.oauth2server.auth.model.User
+import uk.gov.justice.digital.hmpps.oauth2server.delius.model.DeliusUserPersonDetails
+import uk.gov.justice.digital.hmpps.oauth2server.delius.service.DeliusUserService
+import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource.auth
+import uk.gov.justice.digital.hmpps.oauth2server.security.UserDetailsImpl
+
+internal class UserContextServiceTest {
+  private val deliusUserService: DeliusUserService = mock()
+  private val userContextService = UserContextService(deliusUserService)
+
+  @Test
+  fun `resolveUser returns the same user for clients with 'normal' scopes`() {
+    val loginUser = User.builder().username("username").source(auth).build()
+
+    var scopes = setOf("read")
+    var user = userContextService.resolveUser(loginUser, scopes)
+    assertThat(user).isEqualTo(loginUser)
+
+    scopes = setOf("read", "write")
+    user = userContextService.resolveUser(loginUser, scopes)
+    assertThat(user).isEqualTo(loginUser)
+  }
+
+  @Test
+  fun `resolveUser attempts to map user for 'delius' scope but fails due to missing mapping implementation`() {
+    val loginUser = User.builder().username("username").source(auth).build()
+    val scopes = setOf("read", "delius")
+
+    assertThatThrownBy { userContextService.resolveUser(loginUser, scopes) }.isInstanceOf(UserMappingException::class.java)
+  }
+
+  @Test
+  fun `resolveUser returns the same user when attempting to map users to the same auth source`() {
+    val loginUser = UserDetailsImpl("username", "name", listOf(), "delius", "userId", "jwtId")
+    val scopes = setOf("delius")
+
+    val user = userContextService.resolveUser(loginUser, scopes)
+    assertThat(user).isEqualTo(loginUser)
+  }
+
+  @Test
+  fun `resolveUser can map from azureAD to delius`() {
+    val deliusUser = DeliusUserPersonDetails("username", "id", "user", "name", "email@email.com")
+    val loginUser = UserDetailsImpl("username", "name", listOf(), "azuread", "email@email.com", "jwtId")
+    val scopes = setOf("delius")
+    whenever(deliusUserService.getDeliusUserByEmail(anyString())).thenReturn(deliusUser)
+
+    val user = userContextService.resolveUser(loginUser, scopes)
+    assertThat(user).isEqualTo(deliusUser)
+  }
+
+  @Test
+  fun `resolveUser returns the same user when the user service returns a null value`() {
+    val loginUser = UserDetailsImpl("username", "name", listOf(), "azuread", "email@email.com", "jwtId")
+    val scopes = setOf("delius")
+    whenever(deliusUserService.getDeliusUserByEmail("email@email.com")).thenReturn(null)
+
+    val user = userContextService.resolveUser(loginUser, scopes)
+    assertThat(user).isEqualTo(loginUser)
+  }
+}
