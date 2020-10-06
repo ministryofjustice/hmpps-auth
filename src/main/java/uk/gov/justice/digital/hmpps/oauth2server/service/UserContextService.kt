7a75c6302a8c97b8d90afd1621eb0b7d2baf36cf
petergphillips <peter@greenthistle.co.uk>
2020-10-06 14:31:21 +0100

    DT-1179: ✨ Add mapping from azure to external users (#568)

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/service/UserContextService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/service/UserContextService.kt
@@ -1,46 +1,48 @@
 package uk.gov.justice.digital.hmpps.oauth2server.service
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.oauth2server.delius.service.DeliusUserService
+import uk.gov.justice.digital.hmpps.oauth2server.maintain.AuthUserService
 import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource
+import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource.auth
 import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource.azuread
 import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource.delius
 import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource.fromNullableString
 import uk.gov.justice.digital.hmpps.oauth2server.security.UserPersonDetails
 
-class UserMappingException(message: String): Exception(message)
+class UserMappingException(message: String) : Exception(message)
 
 @Service
 class UserContextService(
-    private val deliusUserService: DeliusUserService
-) {
-  companion object {
-    private val log = LoggerFactory.getLogger(UserContextService::class.java)
-  }
-
-  private val deliusScope = "delius"
+    private val deliusUserService: DeliusUserService,
+    private val authUserService: AuthUserService) {
 
   @Throws(UserMappingException::class)
   fun resolveUser(loginUser: UserPersonDetails, scopes: Set<String>): UserPersonDetails {
-    if (scopes.contains(deliusScope)) {
-      return map(loginUser, fromNullableString(loginUser.authSource), delius)
+    val users = discoverUsers(loginUser, scopes)
+    return when (users.size) {
+      1 -> users[0]
+      0 -> loginUser
+      else -> throw UserMappingException("Multiple users found with scopes $scopes")
     }
-
-    return loginUser
   }
 
-  private fun map(user: UserPersonDetails, from: AuthSource, to: AuthSource): UserPersonDetails = when (from) {
-    to -> user
-    azuread -> mapFromAzureAD(user, to)
-    else -> throw UserMappingException("Auth source '${from}' not supported")
+  fun discoverUsers(loginUser: UserPersonDetails, scopes: Set<String>): List<UserPersonDetails> {
+    val loginUserAuthSource = fromNullableString(loginUser.authSource)
+    if (loginUserAuthSource != azuread) return listOf(loginUser)
+
+    val desiredSources = scopes.map { fromNullableString(it) }.filter { it != AuthSource.none }
+    if (desiredSources.isEmpty()) return listOf(loginUser)
+
+    return desiredSources
+        .map { mapFromAzureAD(loginUser, it) }
+        .filter { it.isNotEmpty() }
+        .flatten()
   }
 
-  private fun mapFromAzureAD(user: UserPersonDetails, to: AuthSource): UserPersonDetails = when (to) {
-    delius -> {
-      log.debug("Mapping user context from azure -> delius")
-      deliusUserService.getDeliusUserByEmail(user.userId) ?: user
-    }
-    else -> throw UserMappingException("Auth -> '${to}' mapping not supported")
+  private fun mapFromAzureAD(user: UserPersonDetails, to: AuthSource): List<UserPersonDetails> = when (to) {
+    delius -> deliusUserService.getDeliusUsersByEmail(user.userId)
+    auth -> authUserService.findAuthUsersByEmail(user.userId)
+    else -> emptyList()
   }
 }
