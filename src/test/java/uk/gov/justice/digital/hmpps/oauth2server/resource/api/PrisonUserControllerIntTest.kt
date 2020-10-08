a7e0f36daf28870177f86110a7313a2558b3eda5
bell-pa <peter.bell@digital.justice.gov.uk>
2020-10-08 14:33:27 +0100

    DCS-642 Guard GET /api/prisonuser end-point with role 'ROLE_USE_OF_FORCE'

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/api/PrisonUserControllerIntTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/resource/api/PrisonUserControllerIntTest.kt
@@ -10,7 +10,7 @@ class PrisonUserControllerIntTest : IntegrationTest() {
   fun `prisonuser end-point returns results`() {
     webTestClient
         .get().uri("/auth/api/prisonuser?firstName=ryAn&lastName=OrtoN")
-        .headers(setAuthorisation("UOF_REVIEWER_USER", listOf()))
+        .headers(setAuthorisation("UOF_REVIEWER_USER", listOf("ROLE_USE_OF_FORCE")))
         .exchange()
         .expectStatus().isOk
         .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -28,9 +28,20 @@ class PrisonUserControllerIntTest : IntegrationTest() {
   fun `prisonuser end-point rejects invalid query`() {
     webTestClient
         .get().uri("/auth/api/prisonuser")
-        .headers(setAuthorisation("UOF_REVIEWER_USER", listOf()))
+        .headers(setAuthorisation("UOF_REVIEWER_USER", listOf("ROLE_USE_OF_FORCE")))
         .exchange()
         .expectStatus().is4xxClientError
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+  }
+
+  @Test
+  fun `prisonuser end-point rejects unauthorised request`() {
+    webTestClient
+        .get().uri("/auth/api/prisonuser?firstName=ryAn&lastName=OrtoN")
+        .headers(setAuthorisation("UOF_REVIEWER_USER", listOf()))
+        .exchange()
+        .expectStatus().isForbidden
         .expectHeader().contentType(MediaType.APPLICATION_JSON)
         .expectBody()
   }
