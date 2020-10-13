20cf408abc6c03ca4f5d17b3ae59cf1a44d8ff9e
petergphillips <peter@greenthistle.co.uk>
2020-10-13 14:55:34 +0100

    DT-1201: 🎨 Sort out formatting from previous PR (#586)

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/security/UserContextApprovalHandler.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/security/UserContextApprovalHandler.kt
@@ -9,32 +9,36 @@ import uk.gov.justice.digital.hmpps.oauth2server.security.AuthSource.azuread
 import uk.gov.justice.digital.hmpps.oauth2server.service.UserContextService
 
 class UserContextApprovalHandler(private val userContextService: UserContextService) : TokenStoreUserApprovalHandler() {
-  override fun checkForPreApproval(authorizationRequest: AuthorizationRequest, userAuthentication: Authentication):
-    AuthorizationRequest {
+  override fun checkForPreApproval(
+    authorizationRequest: AuthorizationRequest,
+    userAuthentication: Authentication,
+  ): AuthorizationRequest {
 
-      val approvalRequest = super.checkForPreApproval(authorizationRequest, userAuthentication)
-      if (approvalRequest.isApproved && isAzureAdUser(userAuthentication)) {
-        // force all azure users down the approval route, the controller will work out what accounts are found etc.
-        approvalRequest.isApproved = false
-      }
-
-      return approvalRequest
+    val approvalRequest = super.checkForPreApproval(authorizationRequest, userAuthentication)
+    if (approvalRequest.isApproved && isAzureAdUser(userAuthentication)) {
+      // force all azure users down the approval route, the controller will work out what accounts are found etc.
+      approvalRequest.isApproved = false
     }
+
+    return approvalRequest
+  }
 
   private fun isAzureAdUser(userAuthentication: Authentication) =
     AuthSource.fromNullableString((userAuthentication.principal as UserDetailsImpl).authSource) == azuread
 
-  override fun getUserApprovalRequest(authorizationRequest: AuthorizationRequest, userAuthentication: Authentication):
-    MutableMap<String, Any> {
+  override fun getUserApprovalRequest(
+    authorizationRequest: AuthorizationRequest,
+    userAuthentication: Authentication,
+  ): MutableMap<String, Any> {
 
-      val userApprovalRequest = super.getUserApprovalRequest(authorizationRequest, userAuthentication)
+    val userApprovalRequest = super.getUserApprovalRequest(authorizationRequest, userAuthentication)
 
-      if (isAzureAdUser(userAuthentication)) {
-        val userDetails = userAuthentication.principal as UserPersonDetails
-        val users = userContextService.discoverUsers(userDetails, authorizationRequest.scope)
-        userApprovalRequest["users"] = users
-      }
-
-      return userApprovalRequest
+    if (isAzureAdUser(userAuthentication)) {
+      val userDetails = userAuthentication.principal as UserPersonDetails
+      val users = userContextService.discoverUsers(userDetails, authorizationRequest.scope)
+      userApprovalRequest["users"] = users
     }
+
+    return userApprovalRequest
+  }
 }
