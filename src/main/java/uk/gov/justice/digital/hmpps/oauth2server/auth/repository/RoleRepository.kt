5af467de7f70b6c2caf4605895e071eb909c0889
Peter Phillips <peter@greenthistle.co.uk>
2020-10-20 14:08:43 +0100

    DT-1223: 🎨 Convert java to kotlin

---
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/auth/repository/RoleRepository.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.oauth2server.auth.repository
+
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.lang.NonNull
+import uk.gov.justice.digital.hmpps.oauth2server.auth.model.Authority
+import java.util.Optional
+
+interface RoleRepository : CrudRepository<Authority, String> {
+  @NonNull
+  fun findAllByOrderByRoleName(): List<Authority>
+  fun findByRoleCode(roleCode: String?): Optional<Authority>
+
+  @Query("select distinct r from User u join u.groups g join g.assignableRoles gar join gar.role r where u.username = ?1 order by r.roleName")
+  fun findByGroupAssignableRolesForUsername(username: String?): Set<Authority>
+}
