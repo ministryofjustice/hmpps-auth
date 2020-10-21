c48fc5ac90259199d150fbbc5e1c7cfbb12de942
petergphillips <peter@greenthistle.co.uk>
2020-10-21 10:32:40 +0100

    DT-1225: ✅ Add test for missing scenario (#604)

--- src/test/java/uk/gov/justice/digital/hmpps/oauth2server/security/UserRetriesServiceTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/oauth2server/security/UserRetriesServiceTest.kt
@@ -6,6 +6,7 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.verify
@@ -28,128 +29,159 @@ class UserRetriesServiceTest {
   private val userService: UserService = mock()
   private val service = UserRetriesService(userRetriesRepository, userRepository, delegatingUserService, userService, 3)
 
-  @Test
-  fun resetRetriesAndRecordLogin() {
-    whenever(userService.getEmailAddressFromNomis(anyString())).thenReturn(Optional.of("bob@bob.justice.gov.uk"))
-    service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
-    verify(userRetriesRepository).save<UserRetries>(
-      check {
-        assertThat(it).isEqualTo(UserRetries("bob", 0))
-      }
-    )
-  }
-
-  @Test
-  fun resetRetriesAndRecordLogin_RecordLastLogginIn() {
-    val user = User.builder().username("joe").lastLoggedIn(LocalDateTime.now().minusDays(1)).build()
-    whenever(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user))
-    service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
-    assertThat(user.lastLoggedIn).isBetween(LocalDateTime.now().plusMinutes(-1), LocalDateTime.now())
-  }
-
-  @Test
-  fun resetRetriesAndRecordLogin_SaveDeliusEmailAddress() {
-    val user = User.builder().username("joe").lastLoggedIn(LocalDateTime.now().minusDays(1)).build()
-    whenever(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user))
-    service.resetRetriesAndRecordLogin(
-      DeliusUserPersonDetails(
-        "deliusUser",
-        "12345",
-        "Delius",
-        "Smith",
-        "newemail@bob.com",
-        true,
-        false,
-        emptySet()
+  @Nested
+  inner class resetRetriesAndRecordLogin {
+    @Test
+    fun resetRetriesAndRecordLogin() {
+      whenever(userService.getEmailAddressFromNomis(anyString())).thenReturn(Optional.of("bob@bob.justice.gov.uk"))
+      service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
+      verify(userRetriesRepository).save<UserRetries>(
+        check {
+          assertThat(it).isEqualTo(UserRetries("bob", 0))
+        }
       )
-    )
-    assertThat(user.email).isEqualTo("newemail@bob.com")
-    assertThat(user.isVerified).isTrue()
+    }
+
+    @Test
+    fun resetRetriesAndRecordLogin_RecordLastLogginIn() {
+      val user = User.builder().username("joe").lastLoggedIn(LocalDateTime.now().minusDays(1)).build()
+      whenever(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user))
+      service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
+      assertThat(user.lastLoggedIn).isBetween(LocalDateTime.now().plusMinutes(-1), LocalDateTime.now())
+    }
+
+    @Test
+    fun `resetRetriesAndRecordLogin save delius email address existing user`() {
+      val user = User.builder().username("joe").lastLoggedIn(LocalDateTime.now().minusDays(1)).build()
+      whenever(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user))
+      service.resetRetriesAndRecordLogin(
+        DeliusUserPersonDetails(
+          "deliusUser",
+          "12345",
+          "Delius",
+          "Smith",
+          "newemail@bob.com",
+          true,
+          false,
+          emptySet()
+        )
+      )
+      assertThat(user.email).isEqualTo("newemail@bob.com")
+      assertThat(user.isVerified).isTrue()
+    }
+
+    @Test
+    fun `resetRetriesAndRecordLogin save delius email address new user`() {
+      service.resetRetriesAndRecordLogin(
+        DeliusUserPersonDetails(
+          "deliusUser",
+          "12345",
+          "Delius",
+          "Smith",
+          "newemail@bob.com",
+          true,
+          false,
+          emptySet()
+        )
+      )
+      verify(userRepository).save(
+        check { user ->
+          assertThat(user.email).isEqualTo("newemail@bob.com")
+          assertThat(user.isVerified).isTrue()
+        }
+      )
+    }
+
+    @Test
+    fun resetRetriesAndRecordLogin_SaveNewUserWithNomisEmailVerified() {
+      whenever(userService.getEmailAddressFromNomis(anyString())).thenReturn(Optional.of("bob@bob.justice.gov.uk"))
+      service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
+      verify(userRepository).save<User>(
+        check {
+          assertThat(it.username).isEqualTo("bob")
+          assertThat(it.email).isEqualTo("bob@bob.justice.gov.uk")
+          assertThat(it.isVerified).isTrue()
+          assertThat(it.lastLoggedIn).isBetween(LocalDateTime.now().plusMinutes(-1), LocalDateTime.now())
+        }
+      )
+    }
+
+    @Test
+    fun resetRetriesAndRecordLogin_SaveNewNomisUserNoEmailAsNotJusticeEmail() {
+      whenever(userService.getEmailAddressFromNomis(anyString())).thenReturn(Optional.empty())
+      service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
+      verify(userRepository).save<User>(
+        check {
+          assertThat(it.username).isEqualTo("bob")
+          assertThat(it.isVerified).isFalse()
+          assertThat(it.lastLoggedIn).isBetween(LocalDateTime.now().plusMinutes(-1), LocalDateTime.now())
+        }
+      )
+    }
   }
 
-  @Test
-  fun resetRetriesAndRecordLogin_SaveNewUserWithNomisEmailVerified() {
-    whenever(userService.getEmailAddressFromNomis(anyString())).thenReturn(Optional.of("bob@bob.justice.gov.uk"))
-    service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
-    verify(userRepository).save<User>(
-      check {
-        assertThat(it.username).isEqualTo("bob")
-        assertThat(it.email).isEqualTo("bob@bob.justice.gov.uk")
-        assertThat(it.isVerified).isTrue()
-        assertThat(it.lastLoggedIn).isBetween(LocalDateTime.now().plusMinutes(-1), LocalDateTime.now())
-      }
-    )
+  @Nested
+  inner class incrementRetriesAndLockAccountIfNecessary {
+    @Test
+    fun incrementRetriesAndLockAccountIfNecessary_retriesTo0() {
+      service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)
+      verify(userRetriesRepository).save<UserRetries>(
+        check {
+          assertThat(it).isEqualTo(UserRetries("bob", 0))
+        }
+      )
+    }
+
+    @Test
+    fun incrementRetriesAndLockAccountIfNecessary_moreAttemptsAllowed() {
+      whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.of(UserRetries("bob", 1)))
+      val userPersonDetailsForBob = userPersonDetailsForBob
+      service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)
+      verify(delegatingUserService, never()).lockAccount(any())
+    }
+
+    @Test
+    fun incrementRetriesAndLockAccountIfNecessary_lockAccount() {
+      whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.of(UserRetries("bob", 2)))
+      val userPersonDetailsForBob = userPersonDetailsForBob
+      service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)
+      verify(delegatingUserService).lockAccount(userPersonDetailsForBob)
+    }
+
+    @Test
+    fun incrementRetriesAndLockAccountIfNecessary_NoExistingRow() {
+      whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.empty())
+      assertThat(service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)).isEqualTo(false)
+      verify(userRetriesRepository).save<UserRetries>(
+        check {
+          assertThat(it).isEqualTo(UserRetries("bob", 11))
+        }
+      )
+    }
+
+    @Test
+    fun incrementRetriesAndLockAccountIfNecessary_ExistingRow() {
+      whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.of(UserRetries("bob", 5)))
+      assertThat(service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)).isEqualTo(true)
+      verify(userRetriesRepository).save<UserRetries>(
+        check {
+          assertThat(it).isEqualTo(UserRetries("bob", 6))
+        }
+      )
+    }
   }
 
-  @Test
-  fun resetRetriesAndRecordLogin_SaveNewNomisUserNoEmailAsNotJusticeEmail() {
-    whenever(userService.getEmailAddressFromNomis(anyString())).thenReturn(Optional.empty())
-    service.resetRetriesAndRecordLogin(userPersonDetailsForBob)
-    verify(userRepository).save<User>(
-      check {
-        assertThat(it.username).isEqualTo("bob")
-        assertThat(it.isVerified).isFalse()
-        assertThat(it.lastLoggedIn).isBetween(LocalDateTime.now().plusMinutes(-1), LocalDateTime.now())
-      }
-    )
-  }
-
-  @Test
-  fun incrementRetriesAndLockAccountIfNecessary_retriesTo0() {
-    service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)
-    verify(userRetriesRepository).save<UserRetries>(
-      check {
-        assertThat(it).isEqualTo(UserRetries("bob", 0))
-      }
-    )
-  }
-
-  @Test
-  fun incrementRetriesAndLockAccountIfNecessary_moreAttemptsAllowed() {
-    whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.of(UserRetries("bob", 1)))
-    val userPersonDetailsForBob = userPersonDetailsForBob
-    service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)
-    verify(delegatingUserService, never()).lockAccount(any())
-  }
-
-  @Test
-  fun incrementRetriesAndLockAccountIfNecessary_lockAccount() {
-    whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.of(UserRetries("bob", 2)))
-    val userPersonDetailsForBob = userPersonDetailsForBob
-    service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)
-    verify(delegatingUserService).lockAccount(userPersonDetailsForBob)
-  }
-
-  @Test
-  fun incrementRetriesAndLockAccountIfNecessary_NoExistingRow() {
-    whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.empty())
-    assertThat(service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)).isEqualTo(false)
-    verify(userRetriesRepository).save<UserRetries>(
-      check {
-        assertThat(it).isEqualTo(UserRetries("bob", 11))
-      }
-    )
-  }
-
-  @Test
-  fun incrementRetriesAndLockAccountIfNecessary_ExistingRow() {
-    whenever(userRetriesRepository.findById(anyString())).thenReturn(Optional.of(UserRetries("bob", 5)))
-    assertThat(service.incrementRetriesAndLockAccountIfNecessary(userPersonDetailsForBob)).isEqualTo(true)
-    verify(userRetriesRepository).save<UserRetries>(
-      check {
-        assertThat(it).isEqualTo(UserRetries("bob", 6))
-      }
-    )
-  }
-
-  @Test
-  fun resetRetries() {
-    service.resetRetries("bob")
-    verify(userRetriesRepository).save<UserRetries>(
-      check {
-        assertThat(it).isEqualTo(UserRetries("bob", 0))
-      }
-    )
+  @Nested
+  inner class resetRetries {
+    @Test
+    fun resetRetries() {
+      service.resetRetries("bob")
+      verify(userRetriesRepository).save<UserRetries>(
+        check {
+          assertThat(it).isEqualTo(UserRetries("bob", 0))
+        }
+      )
+    }
   }
 
   private val userPersonDetailsForBob: UserPersonDetails
