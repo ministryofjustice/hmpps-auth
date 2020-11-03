7477972072304bebc1b6a3981d19e8722f833888
dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>
2020-11-03 14:45:46 +0000

    DT-1278 🔧 Converted Java classes to Kotlin

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/SimpleSavedRequest.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/config/SimpleSavedRequest.kt
@@ -9,35 +9,20 @@ import javax.servlet.http.Cookie
  * Taken from https://github.com/AusDTO/spring-security-stateless/blob/master/src/main/java/au/gov/dto/springframework/security/web/savedrequest/SimpleSavedRequest.java
  */
 class SimpleSavedRequest internal constructor(private val redirectUrl: String) : SavedRequest {
-  override fun getRedirectUrl(): String {
-    return redirectUrl
-  }
 
-  override fun getCookies(): List<Cookie> {
-    return emptyList()
-  }
+  override fun getRedirectUrl(): String = redirectUrl
 
-  override fun getMethod(): String {
-    return "GET"
-  }
+  override fun getCookies(): List<Cookie> = emptyList()
 
-  override fun getHeaderValues(name: String): List<String> {
-    return emptyList()
-  }
+  override fun getMethod(): String = "GET"
 
-  override fun getHeaderNames(): Collection<String> {
-    return emptyList()
-  }
+  override fun getHeaderValues(name: String): List<String> = emptyList()
 
-  override fun getLocales(): List<Locale> {
-    return emptyList()
-  }
+  override fun getHeaderNames(): Collection<String> = emptyList()
 
-  override fun getParameterValues(name: String): Array<String> {
-    return emptyArray()
-  }
+  override fun getLocales(): List<Locale> = emptyList()
 
-  override fun getParameterMap(): Map<String, Array<String>> {
-    return emptyMap()
-  }
+  override fun getParameterValues(name: String): Array<String> = emptyArray()
+
+  override fun getParameterMap(): Map<String, Array<String>> = emptyMap()
 }
