3001122fc71fdb26c5d4db3b4134b8b47edd26a5
dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>
2020-11-03 14:45:46 +0000

    DT-1278 🔧 Converted Java classes to Kotlin

--- src/main/java/uk/gov/justice/digital/hmpps/oauth2server/Oauth2serverApplication.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/oauth2server/Oauth2serverApplication.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.oauth2server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -11,3 +13,7 @@ class Oauth2serverApplication
 fun main(args: Array<String>) {
   runApplication<Oauth2serverApplication>(*args)
 }
+
+@Configuration
+@EnableScheduling
+class SchedulingConfiguration
