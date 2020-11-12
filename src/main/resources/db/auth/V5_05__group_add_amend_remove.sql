e4bbf25f74733e924b3b7f33f02995771ea59f40
dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>
2020-11-12 15:54:38 +0000

    DT-1285 added changes to the groups table

---
+++ src/main/resources/db/auth/V5_05__group_add_amend_remove.sql
@@ -0,0 +1,22 @@
+update groups  set group_name='PECS Court BARROW IN FURNESS COMBINED COURT'
+    where group_code='PECS_BARRBC';
+
+update groups  set group_code='PECS_NRTSMC', group_name='PECS Court North Shields Magistrates Court'
+    where group_code='PECS_NORTMC';
+
+
+INSERT INTO groups (group_id, group_code, group_name)
+    VALUES (NEWID(), 'PECS_AMERCC', 'PECS Court Amersham Crown Court');
+
+INSERT INTO group_assignable_role (role_id, group_id, automatic)
+    VALUES ((select role_id from roles where role_code = 'PECS_COURT'),
+        (select group_id from groups where group_code = 'PECS_AMERCC'),
+        1);
+
+INSERT INTO groups (group_id, group_code, group_name)
+    VALUES (NEWID(), 'PECS_CUCOCT', 'PECS Court Surrey Coroners Court');
+
+INSERT INTO group_assignable_role (role_id, group_id, automatic)
+    VALUES ((select role_id from roles where role_code = 'PECS_COURT'),
+        (select group_id from groups where group_code = 'PECS_CUCOCT'),
+        1);
