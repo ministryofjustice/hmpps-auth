0e77f58ad0f14d38b9b1b216de9f1ea6d85570e1
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-11-12 14:27:27 +0000

    P4-2452 correcting the role description.

--- src/main/resources/db/auth/V5_04__pecs_jpc_user_role.sql
+++ src/main/resources/db/auth/V5_04__pecs_jpc_user_role.sql
@@ -1,2 +1,2 @@
 INSERT INTO roles (role_id, role_code, role_name)
-VALUES (newid(), 'PECS_JPC_USER', 'PECS journey price calculation service user');
+VALUES (newid(), 'PECS_JPC_USER', 'PECS Journey Price Calculation');
