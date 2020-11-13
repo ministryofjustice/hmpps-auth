995e8584690f090a65f6849c739a0968db1eac41
dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>
2020-11-13 11:46:05 +0000

    DT-1309 removed northshield change as it is not needed

--- src/main/resources/db/auth/V5_06__group_add_amend_remove.sql
+++ src/main/resources/db/auth/V5_06__group_add_amend_remove.sql
@@ -1,10 +1,6 @@
 update groups  set group_name='PECS Court Barrow In Furness Combined Court'
     where group_code='PECS_BARRBC';
 
-update groups  set group_code='PECS_NRTSMC', group_name='PECS Court North Shields Magistrates Court'
-    where group_code='PECS_NORTMC';
-
-
 INSERT INTO groups (group_id, group_code, group_name)
     VALUES (NEWID(), 'PECS_AMERCC', 'PECS Court Amersham Crown Court');
 
