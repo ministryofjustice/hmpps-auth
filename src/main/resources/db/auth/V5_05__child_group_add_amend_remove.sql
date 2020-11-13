3d11b391588024bd57033e54439ac40de80d7101
dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>
2020-11-13 09:35:26 +0000

    DT-1309 added changes to child group

--- src/main/resources/db/auth/V5_05__child_group_add_amend_remove.sql
+++ src/main/resources/db/auth/V5_05__child_group_add_amend_remove.sql
@@ -11,16 +11,13 @@ update child_group
 update child_group
     set child_group_name='PECS Wakefield District Police Station'
         where child_group_code='PECS_WYP19';
+update child_group
+    set child_group_name='PECS St. Mary''s Wharf Police Station'
+        where child_group_code='PECS_DRB3';
 
 INSERT INTO child_group (child_group_id, child_group_code, child_group_name, group_id)
-    (select newid(), 'PECS_DRB8', 'PECS Ilkeston Police Station', group_id from groups where group_code = 'PECS_DRB');
-
-
+    (select newid(), 'PECS_DRB9', 'PECS Ilkeston Police Station', group_id from groups where group_code = 'PECS_DRB');
 INSERT INTO child_group (child_group_id, child_group_code, child_group_name, group_id)
-    (select newid(), 'PECS_DRB9', 'PECS St. Marys Wharf Police Station', group_id from groups where group_code = 'PECS_DRB');
-
-INSERT INTO child_group (child_group_id, child_group_code, child_group_name, group_id)
-    (select newid(), 'PECS_DP8', 'PECS Braintree Police Station', group_id from groups where group_code = 'PECS_DP');
-
+    (select newid(), 'PECS_ESX8', 'PECS Braintree Police Station', group_id from groups where group_code = 'PECS_ESX');
 INSERT INTO child_group (child_group_id, child_group_code, child_group_name, group_id)
     (select newid(), 'PECS_NFL6',' PECS Norwich City Police Station', group_id from groups where group_code = 'PECS_NFL');
