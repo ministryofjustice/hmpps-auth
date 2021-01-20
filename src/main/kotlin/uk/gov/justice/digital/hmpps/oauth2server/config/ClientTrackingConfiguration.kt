11a92cc3418c48ea44edacab43df10c461e9b557
petergphillips <peter@greenthistle.co.uk>
2021-01-20 16:14:36 +0000

    DT-1470: ⬆️🚧 Fix custom dimensions not appearing (#738)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/config/ClientTrackingConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/config/ClientTrackingConfiguration.kt
@@ -5,13 +5,12 @@ import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
 import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import uk.gov.justice.digital.hmpps.oauth2server.config.ApplicationInsightsConfiguration.AppInsightsConfigurationPresent
 import uk.gov.justice.digital.hmpps.oauth2server.utils.IpAddressHelper
 import java.text.ParseException
 import java.util.Optional
@@ -19,7 +18,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Configuration
-@ConditionalOnBean(AppInsightsConfigurationPresent::class)
+@ConditionalOnExpression("T(org.apache.commons.lang3.StringUtils).isNotBlank('\${applicationinsights.connection.string:}')")
 class ClientTrackingConfiguration(private val clientTrackingInterceptor: ClientTrackingInterceptor) : WebMvcConfigurer {
   override fun addInterceptors(registry: InterceptorRegistry) {
     log.info("Adding application insights client tracking interceptor")
@@ -36,7 +35,6 @@ class ClientTrackingInterceptor : HandlerInterceptor {
   override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
     val properties = ThreadContext.getRequestTelemetryContext().httpRequestTelemetry.properties
     val addr = IpAddressHelper.retrieveIpFromRemoteAddr(request)
-    log.info("Setting client ip address of {} in the request telemetry context for application insights", addr)
     properties["clientIpAddress"] = addr
 
     val token = request.getHeader(HttpHeaders.AUTHORIZATION)
