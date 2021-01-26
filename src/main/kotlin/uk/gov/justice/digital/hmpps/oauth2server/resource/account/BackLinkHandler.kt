dc1e01ea146fa57b3b6a7db0db3ee29ca34b7f4e
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2021-01-26 09:01:21 +0000

    DT-1505: refactored backlink handler

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/account/BackLinkHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/account/BackLinkHandler.kt
@@ -8,7 +8,6 @@ import org.springframework.security.oauth2.provider.ClientDetailsService
 import org.springframework.security.oauth2.provider.endpoint.DefaultRedirectResolver
 import org.springframework.security.oauth2.provider.endpoint.RedirectResolver
 import org.springframework.stereotype.Component
-import org.springframework.util.CollectionUtils
 import java.net.URI
 
 @Suppress("DEPRECATION")
@@ -31,44 +30,31 @@ class BackLinkHandler(
     private val log = LoggerFactory.getLogger(BackLinkHandler::class.java)
   }
 
-  fun validateRedirect(
-    client: String,
-    redirect: String,
-  ): Boolean {
+  fun validateRedirect(client: String, redirect: String): Boolean {
 
-    val uri = URI(redirect)
-    val url = when {
-      uri.port == 443 -> "${uri.scheme}://${uri.host}"
-      uri.port != -1 -> "${uri.scheme}://${uri.host}:${uri.port}"
-      else -> "${uri.scheme}://${uri.host}"
+    val url = with(URI(redirect)) {
+      when (port) {
+        -1, 80, 443 -> "$scheme://$host"
+        else -> "$scheme://$host:$port"
+      }
     }
-
     // If we have asked for a redirect, check it is valid for the client
     val clientDetails = clientDetailsService.loadClientByClientId(client)
 
-    if (clientDetails != null && !CollectionUtils.isEmpty(clientDetails.registeredRedirectUri)) {
-      if (responseRedirectedOnValidRedirect(url, clientDetails)) {
-        return true
-      }
-      // second attempt - add trailing slash
-      if (responseRedirectedOnValidRedirect("$url/", clientDetails)) {
-        return true
-      }
-    }
-    return false
+    return clientDetails?.registeredRedirectUri?.isNotEmpty() == true &&
+      (
+        responseRedirectedOnValidRedirect(url, clientDetails) || responseRedirectedOnValidRedirect(
+          "$url/",
+          clientDetails
+        )
+        )
   }
 
-  private fun responseRedirectedOnValidRedirect(
-    redirect: String,
-    clientDetails: ClientDetails
-  ): Boolean {
-    try {
-
-      redirectResolver.resolveRedirect(redirect, clientDetails)
-      return true
-    } catch (rme: RedirectMismatchException) {
-      log.info("Ignoring redirect {} as not valid for client {}, {}", redirect, clientDetails.clientId, rme.message)
-    }
-    return false
+  private fun responseRedirectedOnValidRedirect(redirect: String, clientDetails: ClientDetails) = try {
+    redirectResolver.resolveRedirect(redirect, clientDetails)
+    true
+  } catch (rme: RedirectMismatchException) {
+    log.info("Ignoring redirect {} as not valid for client {}, {}", redirect, clientDetails.clientId, rme.message)
+    false
   }
 }
