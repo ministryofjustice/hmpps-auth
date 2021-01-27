d23616101fc8678df1ab25ae68cfdf0f5661b38b
dnyanesh-sonwane <73168863+dnyanesh-sonwane@users.noreply.github.com>
2021-01-27 15:57:19 +0000

    Dt 1450 remove group (#745)
    * DT-1450 Removed a group
    
    * DT-1450 Removed a group
    
    Co-authored-by: dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>

---
+++ src/main/resources/db/auth/V5_20__pecs_group_removals.sql
@@ -0,0 +1,17 @@
+delete
+from user_group
+where group_id in
+      (select group_id
+       from groups
+       where group_code = 'PECS_STTCYC');
+
+delete
+from group_assignable_role
+where group_id in
+      (select group_id
+       from groups
+       where group_code = 'PECS_STTCYC');
+
+delete
+from groups
+where group_code = 'PECS_STTCYC';
