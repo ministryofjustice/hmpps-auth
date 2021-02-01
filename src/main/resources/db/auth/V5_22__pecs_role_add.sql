e17efd8b39a0dd2e4b1514b398fcf9272a608ca2
dnyanesh-sonwane <73168863+dnyanesh-sonwane@users.noreply.github.com>
2021-02-01 15:32:09 +0000

    DT-1520 new roles (#751)
    Co-authored-by: dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>

---
+++ src/main/resources/db/auth/V5_22__pecs_role_add.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (role_id, role_code, role_name)
+VALUES (newid(), 'PECS_CDM', 'PECS Contract Delivery Manager'),
+       (newid(), 'PECS_SYSTEM_ADMIN', 'PECS System Administrator');
