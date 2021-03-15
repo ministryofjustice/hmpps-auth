90364056b5f7f130aa5aa0cfef545f6f26e312f9
petergphillips <peter@greenthistle.co.uk>
2021-03-15 14:55:15 +0000

    DT-1751: ✨ Add cache control headers to jwk endpoints (#812)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/AuthServerMetadataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/AuthServerMetadataController.kt
@@ -3,13 +3,15 @@ package uk.gov.justice.digital.hmpps.oauth2server.resource.api
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class AuthServerMetadataController {
 
   @GetMapping("/issuer/.well-known/openid-configuration")
-  fun metadata(): Map<String, Any> {
+  fun metadata(response: HttpServletResponse): Map<String, Any> {
     val baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString()
+    response.addHeader("Cache-Control", "max-age=43200, public")
     return mapOf(
       "issuer" to "$baseUrl/issuer",
       "authorization_endpoint" to "$baseUrl/oauth/authorize",
