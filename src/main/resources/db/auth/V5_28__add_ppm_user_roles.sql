fb27803bd71b029eece659522cf53600de947228
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2021-03-30 17:24:09 +0100

    🗃 PPD-210: Create PPM roles in auth

---
+++ src/main/resources/db/auth/V5_28__add_ppm_user_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles (role_id, role_code, role_name)
+VALUES (newid(), 'PPM_ANALYST',             'PPM Analyst'),
+       (newid(), 'PPM_AUTHORISING_OFFICER', 'PPM Authorising Officer'),
+       (newid(), 'PPM_GLOBAL_ADMIN',        'PPM Global Admin');
