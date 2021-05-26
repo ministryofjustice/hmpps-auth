58c59e10255b7644ed543c23ed18bf652e75c696
Peter Phillips <peter@greenthistle.co.uk>
2021-05-26 10:47:06 +0100

    DT-2070: 🐛 Regen stored token as spring upgrade causes different serialization class format

--- src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/OauthIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/OauthIntTest.kt
@@ -23,9 +23,9 @@ class OauthIntTest : IntegrationTest() {
 
   @Test
   fun `Existing auth code stored in database can be redeemed for access token`() {
-    // from database oauth_code table.  To regenerate - put a breakpoint in ClientLoginSpecification just before the
-    // call to get the access token.  Then go to the /auth/h2-console (blank username or password) and look at the last
-    // row in the oauth_code table
+    // from database oauth_code table.  To regenerate - put a breakpoint in AbstractAuthSpecification.getAccessToken
+    // just before the call to get the access token.  Then go to the /auth/h2-console (blank username or password) and
+    // look at the last row in the oauth_code table
     val authCode = "5bDHCW"
     val clientUrl = "http://localhost:8081/login" // same as row in oauth_code table
     webTestClient
@@ -35,9 +35,9 @@ class OauthIntTest : IntegrationTest() {
       .expectStatus().isOk
       .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
       .expectBody()
-      .jsonPath(".user_name").isEqualTo("ITAG_USER_ADM")
+      .jsonPath(".user_name").isEqualTo("ITAG_USER")
       .jsonPath(".user_id").isEqualTo("1")
-      .jsonPath(".sub").isEqualTo("ITAG_USER_ADM")
+      .jsonPath(".sub").isEqualTo("ITAG_USER")
       .jsonPath(".auth_source").isEqualTo("nomis")
   }
 
