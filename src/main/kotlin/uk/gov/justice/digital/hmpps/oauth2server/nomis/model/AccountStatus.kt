afcc0c988d3be254347aa319e61fd26fae740ed7
karenmillermoj <karen.miller@digital.justice.gov.uk>
2021-06-14 11:16:14 +0100

    DT-2155 Ensure Nomis Locked User can reset password

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/nomis/model/AccountStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/nomis/model/AccountStatus.kt
@@ -15,15 +15,15 @@ enum class AccountStatus(
   EXPIRED(1, "EXPIRED", true, false, false, false),
   EXPIRED_GRACE(2, "EXPIRED(GRACE)", true, false, true, false),
   LOCKED_TIMED(4, "LOCKED(TIMED)", false, true, false, true),
-  LOCKED(8, "LOCKED", false, true, false, false),
+  LOCKED(8, "LOCKED", false, true, false, true),
   EXPIRED_LOCKED_TIMED(5, "EXPIRED & LOCKED(TIMED)", true, true, false, true),
   EXPIRED_GRACE_LOCKED_TIMED(6, "EXPIRED(GRACE) & LOCKED(TIMED)", true, true, true, true),
   EXPIRED_LOCKED(9, "EXPIRED & LOCKED", true, true, false, false),
   EXPIRED_GRACE_LOCKED(10, "EXPIRED(GRACE) & LOCKED", true, true, true, false);
 
   companion object {
-    fun get(code: Int): AccountStatus = values().filter { it.code == code }.first()
+    fun get(code: Int): AccountStatus = values().first { it.code == code }
 
-    fun get(desc: String): AccountStatus = values().filter { it.desc == desc }.first()
+    fun get(desc: String): AccountStatus = values().first { it.desc == desc }
   }
 }
