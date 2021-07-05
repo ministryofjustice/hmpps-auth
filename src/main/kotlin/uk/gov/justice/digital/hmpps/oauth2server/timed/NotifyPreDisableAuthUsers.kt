343aea942f7654a6c09b8ddf0a291d1412e93c62
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2021-07-05 21:51:47 +0100

    DT-2097: disable notify pre-disable in preprod (#935)
    * DT-2097: added environment variable to enable/disable notify pre-disable
    
    * DT-2097: updated formatting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/timed/NotifyPreDisableAuthUsers.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/timed/NotifyPreDisableAuthUsers.kt
@@ -2,14 +2,19 @@ package uk.gov.justice.digital.hmpps.oauth2server.timed
 
 import com.microsoft.applicationinsights.TelemetryClient
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 /**
- * notify users that haven't logged into the system for 83 days they will be disabled in 7 days
+ * Notify users that haven't logged into the system for 83 days their account will be disabled in 7 days
  */
 @Component
-class NotifyPreDisableAuthUsers(service: NotifyPreDisableAuthUsersService, telemetryClient: TelemetryClient) :
+class NotifyPreDisableAuthUsers(
+  service: NotifyPreDisableAuthUsersService,
+  telemetryClient: TelemetryClient,
+  @Value("\${application.authentication.notify-pre-disable.enabled}") private val notificationEnabled: Boolean
+) :
   BatchUserProcessor(service, telemetryClient, "NotifyPreDisableInactiveAuthUsers") {
 
   companion object {
@@ -20,13 +25,16 @@ class NotifyPreDisableAuthUsers(service: NotifyPreDisableAuthUsersService, telem
     fixedDelayString = "\${application.authentication.notify-pre-disable.frequency}",
     initialDelayString = "\${random.int[3600000,\${application.authentication.notify-pre-disable.frequency}]}"
   )
-  fun findAndNotifyPreDisableInactiveAuthUsers() {
-    log.info("Notify PreDisable inactive auth users started")
-    val state = findAndProcessInBatches()
-    log.info(
-      "Notify PreDisable inactive auth users finished, processed {} records with {} errors",
-      state.totalAsString,
-      state.errorCountAsString
-    )
-  }
+  fun findAndNotifyPreDisableInactiveAuthUsers() = // disabled in preprod so notification not sent
+    if (notificationEnabled) {
+      log.info("Notify PreDisable inactive auth users started")
+      val state = findAndProcessInBatches()
+      log.info(
+        "Notify PreDisable inactive auth users finished, processed {} records with {} errors",
+        state.totalAsString,
+        state.errorCountAsString
+      )
+    } else {
+      log.info("Notify PreDisable inactive auth users disabled in this environment")
+    }
 }
