efa9aaee52e953201c6623e33d6d9f2b1f25d96d
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2021-07-22 12:15:18 +0100

    DT-2194: new endpoint to add/remove roles by userId and get assignable roles by userId

--- src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/AuthUserRolesController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/oauth2server/resource/api/AuthUserRolesController.kt
@@ -106,6 +106,28 @@ class AuthUserRolesController(
     return roles.map { AuthUserRole(it) }
   }
 
+  @GetMapping("/api/authuser/id/{userId}/assignable-roles")
+  @ApiOperation(
+    value = "Get list of assignable roles.",
+    notes = "Get list of roles that can be assigned by the current user.  This is dependent on the group membership, although super users can assign any role",
+    nickname = "assignableRoles",
+    consumes = "application/json",
+    produces = "application/json"
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(code = 401, message = "Unauthorized.", response = ErrorDetail::class),
+      ApiResponse(code = 404, message = "User not found.", response = ErrorDetail::class)
+    ]
+  )
+  fun assignableRolesByUserId(
+    @ApiParam(value = "The userId of the user.", required = true) @PathVariable userId: String,
+    @ApiIgnore authentication: Authentication,
+  ): List<AuthUserRole> {
+    val roles = authUserRoleService.getAssignableRolesByUserId(userId, authentication.authorities)
+    return roles.map { AuthUserRole(it) }
+  }
+
   @PutMapping("/api/authuser/{username}/roles/{role}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @PreAuthorize("hasAnyRole('ROLE_MAINTAIN_OAUTH_USERS', 'ROLE_AUTH_GROUP_MANAGER')")
@@ -135,6 +157,34 @@ class AuthUserRolesController(
     val usernameInDb = user.username
     authUserRoleService.addRoles(usernameInDb, listOf(role), authentication.name, authentication.authorities)
     log.info("Add role succeeded for user {} and role {}", usernameInDb, role)
+  }
+
+  @PutMapping("/api/authuser/id/{userId}/roles/{role}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PreAuthorize("hasAnyRole('ROLE_MAINTAIN_OAUTH_USERS', 'ROLE_AUTH_GROUP_MANAGER')")
+  @ApiOperation(
+    value = "Add role to user.",
+    notes = "Add role to user.",
+    nickname = "addRole",
+    consumes = "application/json",
+    produces = "application/json"
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(code = 400, message = "Validation failed.", response = ErrorDetail::class),
+      ApiResponse(code = 401, message = "Unauthorized.", response = ErrorDetail::class),
+      ApiResponse(code = 404, message = "User not found.", response = ErrorDetail::class),
+      ApiResponse(code = 409, message = "Role for user already exists.", response = ErrorDetail::class),
+      ApiResponse(code = 500, message = "Server exception e.g. failed to insert row.", response = ErrorDetail::class)
+    ]
+  )
+  fun addRoleByUserId(
+    @ApiParam(value = "The userId of the user.", required = true) @PathVariable userId: String,
+    @ApiParam(value = "The role to be added to the user.", required = true) @PathVariable role: String,
+    @ApiIgnore authentication: Authentication,
+  ) {
+    authUserRoleService.addRolesByUserId(userId, listOf(role), authentication.name, authentication.authorities)
+    log.info("Add role succeeded for userId {} and role {}", userId, role)
   }
 
   @DeleteMapping("/api/authuser/{username}/roles/{role}")
@@ -168,6 +218,34 @@ class AuthUserRolesController(
     log.info("Remove role succeeded for user {} and role {}", usernameInDb, role)
   }
 
+  @DeleteMapping("/api/authuser/id/{userId}/roles/{role}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PreAuthorize("hasAnyRole('ROLE_MAINTAIN_OAUTH_USERS', 'ROLE_AUTH_GROUP_MANAGER')")
+  @ApiOperation(
+    value = "Remove role from user.",
+    notes = "Remove role from user.",
+    nickname = "removeRole",
+    consumes = "application/json",
+    produces = "application/json"
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(code = 204, message = "Removed"),
+      ApiResponse(code = 400, message = "Validation failed.", response = ErrorDetail::class),
+      ApiResponse(code = 401, message = "Unauthorized.", response = ErrorDetail::class),
+      ApiResponse(code = 404, message = "User not found.", response = ErrorDetail::class),
+      ApiResponse(code = 500, message = "Server exception e.g. failed to insert row.", response = ErrorDetail::class)
+    ]
+  )
+  fun removeRoleByUserId(
+    @ApiParam(value = "The userId of the user.", required = true) @PathVariable userId: String,
+    @ApiParam(value = "The role to be delete from the user.", required = true) @PathVariable role: String,
+    @ApiIgnore authentication: Authentication,
+  ) {
+    authUserRoleService.removeRoleByUserId(userId, role, authentication.name, authentication.authorities)
+    log.info("Remove role succeeded for userId {} and role {}", userId, role)
+  }
+
   @PostMapping("/api/authuser/{username}/roles")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @PreAuthorize("hasAnyRole('ROLE_MAINTAIN_OAUTH_USERS', 'ROLE_AUTH_GROUP_MANAGER')")
@@ -197,6 +275,34 @@ class AuthUserRolesController(
     val usernameInDb = user.username
     authUserRoleService.addRoles(usernameInDb, roles, authentication.name, authentication.authorities)
     log.info("Add role succeeded for user {} and roles {}", usernameInDb, roles.toString())
+  }
+
+  @PostMapping("/api/authuser/id/{userId}/roles")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PreAuthorize("hasAnyRole('ROLE_MAINTAIN_OAUTH_USERS', 'ROLE_AUTH_GROUP_MANAGER')")
+  @ApiOperation(
+    value = "Add roles to user.",
+    notes = "Add role to user, post version taking multiple roles",
+    nickname = "addRole",
+    consumes = "application/json",
+    produces = "application/json"
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(code = 400, message = "Validation failed.", response = ErrorDetail::class),
+      ApiResponse(code = 401, message = "Unauthorized.", response = ErrorDetail::class),
+      ApiResponse(code = 404, message = "User not found.", response = ErrorDetail::class),
+      ApiResponse(code = 409, message = "Role(s) for user already exists.", response = ErrorDetail::class),
+      ApiResponse(code = 500, message = "Server exception e.g. failed to insert row.", response = ErrorDetail::class)
+    ]
+  )
+  fun addRolesByUserId(
+    @ApiParam(value = "The username of the user.", required = true) @PathVariable userId: String,
+    @ApiParam(value = "List of roles to be assigned.", required = true) @RequestBody @NotEmpty roles: List<String>,
+    @ApiIgnore authentication: Authentication,
+  ) {
+    authUserRoleService.addRolesByUserId(userId, roles, authentication.name, authentication.authorities)
+    log.info("Add role succeeded for userId {} and roles {}", userId, roles.toString())
   }
 
   companion object {
